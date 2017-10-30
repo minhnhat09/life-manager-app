b35bc3296572f7fb1070e3b82772e7fbf0ba6b48
minhnhat09 <nhatminh1809@gmail.com>
2017-10-30 10:18:08 +0100

    add proptype to spending row

--- client/src/components/spending/SpendingRow.js
+++ client/src/components/spending/SpendingRow.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from 'prop-types';
 const SpendingRow = ({ name, amount, type, date }) => (
   <tr>
     <td>{name}</td>
@@ -8,4 +8,11 @@ const SpendingRow = ({ name, amount, type, date }) => (
     <td>{date}</td>
   </tr>
 );
+
+SpendingRow.propTypes = {
+  name: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date)
+};
 export default SpendingRow;
