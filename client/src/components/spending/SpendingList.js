3f6a83375de8e6276c2b752f53c4a49c694397e7
minhnhat09 <nhatminh1809@gmail.com>
2017-10-30 16:36:18 +0100

    add material ui table

--- client/src/components/spending/SpendingList.js
+++ client/src/components/spending/SpendingList.js
@@ -1,21 +1,44 @@
 import React from "react";
 import SpendingRow from "./SpendingRow";
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from "material-ui/Table";
+import Paper from "material-ui/Paper";
+import { withStyles } from "material-ui/styles";
 
-const SpendingList = ({ spendings }) => {
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 700
+  }
+});
+
+const SpendingList = (props) => {
+  const { classes, spendings } = props;
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Amount</th>
-          <th>Type</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
+
+    <Paper className={classes.root}>
+    <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
         {spendings.map((spending, i) => <SpendingRow key={i} {...spending} />)}
-      </tbody>
-    </table>
+        </TableBody>
+        </Table>
+    </Paper>
   );
 };
-export default SpendingList;
+export default withStyles(styles)(SpendingList);
--- client/src/components/spending/SpendingRow.js
+++ client/src/components/spending/SpendingRow.js
@@ -1,17 +1,24 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from "material-ui/Table";
+
 const SpendingRow = ({ name, amount, type, date }) => (
-  <tr>
-    <td>{name}</td>
-    <td>{amount}</td>
-    <td>{type}</td>
-    <td>{date}</td>
-  </tr>
+  <TableRow>
+    <TableCell>{name}</TableCell>
+    <TableCell>{amount}</TableCell>
+    <TableCell>{type}</TableCell>
+    <TableCell>{date}</TableCell>
+  </TableRow>
 );
 
 SpendingRow.propTypes = {
   name: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-  type: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
 };
 export default SpendingRow;
